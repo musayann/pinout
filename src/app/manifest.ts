0e74221c5fd50b76e0ce76cf6581436a486cc882
Yannick Musafiri <yannick@musafiri.co>
2026-04-14 14:12:06 +0300

    chore: adjust assets

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -30,8 +30,19 @@ export default function manifest(): MetadataRoute.Manifest {
       },
       {
         src: "/favicon.svg",
-        sizes: "any",
         type: "image/svg+xml",
+      },
+      {
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
   };
